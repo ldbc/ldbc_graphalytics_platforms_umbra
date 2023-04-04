fce0c1e5fc533344d9a5b6810dc76eee77f001a5
Gabor Szarnyas <szarnyasg@gmail.com>
2023-04-04 12:53:20 +0200

    Bump Graphalytics framework version to 1.9.0

--- scripts/package-and-run-benchmark.sh
+++ scripts/package-and-run-benchmark.sh
@@ -7,7 +7,7 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd 
 cd ..
 
 scripts/init.sh
-cd graphalytics-1.8.0-umbra-0.0.1-SNAPSHOT
+cd graphalytics-1.9.0-umbra-0.0.1-SNAPSHOT
 
 export POSTGRES_INPUT_DATA_DIR=${POSTGRES_INPUT_DATA_DIR:-~/graphs}
 bin/scripts/start-postgres.sh

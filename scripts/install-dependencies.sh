10901f2febc0b841ddee161f42c432e1cd16337e
Gabor Szarnyas <szarnyasg@gmail.com>
2023-04-22 15:41:56 +0200

    Add Maven among the project's dependencies

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -7,10 +7,10 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
 if [[ ! -z $(which yum) ]]; then
-    sudo yum install -y python3-pip postgresql-devel postgresql
+    sudo yum install -y maven python3-pip postgresql-devel postgresql
 elif [[ ! -z $(which apt-get) ]]; then
     sudo apt-get update
-    sudo apt-get install -y python3-pip libpq5 postgresql-client
+    sudo apt-get install -y maven python3-pip libpq5 postgresql-client
 fi
 
 pip3 install --user --progress-bar off psycopg
